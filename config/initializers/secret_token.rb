b91d3317d024bd440b996063ced5774cfe3f84c7
jamesdeangelis <james@pollenizer.com>
2013-06-05 14:52:08 +1000

    Beginning of project with status update

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Treehouse::Application.config.secret_token = 'beef8934f0459dc8ede69b36da92ea9b1f5065f0533484e54aa64b376927ad568707f2ca2d2827d2ab274797b22e88e72ddbca0651a2559ce894297d2f64be44'
